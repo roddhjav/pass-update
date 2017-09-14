99d761e3d7bb9f3d455be989e2adffb02c556574
Alexandre Pujol <alexandre@pujol.io>
2017-09-14 18:48:38 +0100

    Add unit tests for new features (-p, -m, folder support...)

--- tests/00_update.sh
+++ tests/00_update.sh
@@ -15,6 +15,7 @@ test_expect_success 'Updating a password' '
 
 test_expect_success 'Updating multiple password' '
     _pass update --force France/bank France/freebox France/mobilephone &&
+    _pass update --force Email/
     '
 
 test_expect_success 'Updating password with specific length' '
@@ -31,8 +32,29 @@ if test_have_prereq XCLIP; then
         '
 fi
 
+testing_password_notmatching() {
+    echo -e "pass\ndummypass" | _pass update --force --provide Business/site.com
+}
+test_expect_success 'Updating password with a provided password' '
+    echo -e "dummypass\ndummypass" | _pass update --force --provide Business/site.com &&
+    test_must_fail testing_password_notmatching
+    '
+
+test_expect_success 'Updating a multiline password' '
+    echo -e "dummypass\nlogin: dummylogin" | _pass update --force --multiline Business/site.eu
+    '
+
+test_expect_success 'Testing corner cases' '
+    test_must_fail _pass update --not-an-option &&
+    test_must_fail _pass update --force --provide --clip Email/zx2c4.com &&
+    test_must_fail _pass update --force --length number Email/zx2c4.com &&
+    test_must_fail _pass update --force --provide --multiline Email/zx2c4.com &&
+    _pass update --force Business/site1.com Business/site2.com Email/site3.com Socials/
+    '
+
 test_expect_success 'Testing help message' '
-	_pass update --help
-	'
+    _pass update --help | grep "pass update" &&
+    _pass update --version | grep "A pass extension extension that provides an"
+    '
 
 test_done
